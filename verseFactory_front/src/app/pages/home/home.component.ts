7b6c12cb4519ab086c063ba65cb888051e379120
quentin.lyoen.etu <quentin.lyoen.etu@univ-lille.fr>
2026-07-15 13:24:49 +0200

    feat: implement home page with welcome section and authentication-based play navigation

--- verseFactory_front/src/app/pages/home/home.component.ts
+++ verseFactory_front/src/app/pages/home/home.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+  private titleService = inject(Title);
+  private metaService = inject(Meta);
+  private router = inject(Router);
+  public authService = inject(AuthService);
+
+  ngOnInit() {
+    this.titleService.setTitle('Accueil - VerseFactory');
+    this.metaService.updateTag({ name: 'description', content: 'Découvrez Verse Factory, l\'ultime plateforme pour gérer votre propre usine multidimensionnel.' });
+  }
+
+  public playIfLoggedIn() {
+    if (this.authService.isLoggedIn) {
+      this.router.navigate(['/game'])
+    } else {
+      this.authService.login()
+    }
+  }
+}
