2672baa5138e71a04344307a8a49290362cade13
quentin.lyoen.etu <quentin.lyoen.etu@univ-lille.fr>
2026-07-15 12:29:57 +0200

    feat: (front) create home page component

--- verseFactory_front/src/app/pages/home/home.component.ts
+++ verseFactory_front/src/app/pages/home/home.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <div style="padding: 2rem;">
-      <h1>Bienvenue sur Verse Factory</h1>
-      <p>Cette page est accessible publiquement sans connexion.</p>
-    </div>
-  `
+  templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent { }
