652b6993a93f6999fd817451d6921988efda2112
quentin.lyoen.etu <quentin.lyoen.etu@univ-lille.fr>
2026-07-22 12:07:31 +0200

    feat: (front) create account with navigation bar

--- verseFactory_front/src/app/shared/navigation-bar/navigation-bar.component.ts
+++ verseFactory_front/src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
+import { Dialog } from "@angular/cdk/dialog";
+import { CreateAccountComponent } from "../create-account/create-account.component";
 
 @Component({
     selector: "app-navigation-bar",
@@ -10,4 +12,9 @@ import { AuthService } from "../../services/auth.service";
 
 export class NavigationBarComponent {
     public authService = inject(AuthService);
+    private dialog = inject(Dialog);
+
+    public openCreateAccount(): void {
+        this.dialog.open(CreateAccountComponent);
+    }
 }
