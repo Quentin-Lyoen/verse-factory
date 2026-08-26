3666c3f7794f9aee8d15067fd43c545f89de326e
quentin.lyoen.etu <quentinlyoen03@gmail.com>
2026-08-26 12:14:58 +0200

    feat: switch between box-shop & upgrade-shop

---
+++ verseFactory_front/src/app/pages/shop/pages/upgrade-shop/upgrade-shop.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-upgrade-shop',
+    templateUrl: './upgrade-shop.component.html',
+})
+export class UpgradeShopComponent {
+}
--- verseFactory_front/src/app/pages/shop/shop.component.ts
+++ verseFactory_front/src/app/pages/shop/shop.component.ts
@@ -1,25 +1,31 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 import { Meta, Title } from "@angular/platform-browser";
 import { GameMenuComponent } from "../../shared/game-menu/game-menu.component";
 import { BoxShopComponent } from "./pages/box-shop/box-shop.component";
 import { FactoryService } from "../../services/factory.service";
 import { toSignal } from "@angular/core/rxjs-interop";
+import { UpgradeShopComponent } from "./pages/upgrade-shop/upgrade-shop.component";
 
 @Component({
     selector: 'app-shop',
     templateUrl: './shop.component.html',
-    imports: [GameMenuComponent, BoxShopComponent],
+    imports: [GameMenuComponent, BoxShopComponent, UpgradeShopComponent],
 })
 export class ShopComponent implements OnInit {
     private titleService = inject(Title);
     private metaService = inject(Meta);
     private factoryService = inject(FactoryService);
 
+    public currentShopPage = signal<string>('box');
     
     public factory = toSignal(this.factoryService.getCurrentFactory());
     
     ngOnInit() {
         this.titleService.setTitle('Boutique - VerseFactory');
         this.metaService.updateTag({ name: 'description', content: 'Découvrez Verse Factory, l\'ultime plateforme pour gérer votre propre usine multidimensionnel.' });
+    }
+
+    public switchShopPage(page: string) {
+        this.currentShopPage.set(page);
     }
 }
